5c521f08c053976cbaccbac533c7aff1e993df08
dvenkatesan-tmx <dharshan.venkatesan@tmx.com>
2024-11-19 09:40:08 -0500

    Update mx_dag1.py

--- dags/mx_dag1.py
+++ dags/mx_dag1.py
@@ -5,7 +5,7 @@ from airflow.operators.python_operator import PythonOperator
 
 # Function to be executed by PythonOperator
 def print_hello():
-    print("Hello, Airflow!")
+    print("Airflow!")
 
 # Default arguments for the DAG
 default_args = {
